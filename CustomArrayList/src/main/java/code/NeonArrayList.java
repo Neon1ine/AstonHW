3fdc247f78c2b2f9540a5fcf9d03021eb03becf5
Neon1ine <azeemin1548@yandex.ru>
2024-07-31 21:36:50 +0300

    add remove, clear,get, sort methods

--- CustomArrayList/src/main/java/code/NeonArrayList.java
+++ CustomArrayList/src/main/java/code/NeonArrayList.java
@@ -1,23 +1,23 @@
 package code;
 
-import java.util.Collection;
-import java.util.Comparator;
+import java.util.*;
 
 public class NeonArrayList<E> {
 
     private final int DEFAULT_SIZE = 8;
     private Object[] arr;
     private int size = 0;
+    private int modCount = 0;
 
     public NeonArrayList(int initialCapacity) {
         if (initialCapacity < 0) {
             throw new IllegalArgumentException("Illegal Capacity: " + initialCapacity);
         }
-        arr = new Object[initialCapacity];
+        this.arr = new Object[initialCapacity];
     }
 
     public NeonArrayList() {
-        arr = new Object[DEFAULT_SIZE];
+        this.arr = new Object[DEFAULT_SIZE];
     }
 
     public int size() {
@@ -29,20 +29,22 @@ public class NeonArrayList<E> {
     }
 
     public void add(int index, E element) {
-        if (this.size == arr.length - 1) {
-            extendArray(arr.length * 2);
+        if (this.size > this.arr.length / 2) {
+            extendArray(this.arr.length * 2);
         }
         this.size++;
-        arr[index] = element;
+        this.arr[index] = element;
+        ++this.modCount;
     }
 
     private void extendArray(int newSize) {
         Object[] extendedArr = new Object[newSize];
-        System.arraycopy(arr, 0, extendedArr, 0, this.size);
-        arr = extendedArr;
+        System.arraycopy(this.arr, 0, extendedArr, 0, this.size);
+        this.arr = extendedArr;
     }
 
     public boolean addAll(Collection<? extends E> c) {
+        ++this.modCount;
         Object[] a = c.toArray();
         if (a.length == 0) {
             return false;
@@ -57,24 +59,104 @@ public class NeonArrayList<E> {
         }
     }
 
-    //todo implement methods
-    public Object remove(int index) {
-        return null;
-    }
-
-    public boolean remove(Object o) {
-        return false;
-    }
-
     public void clear() {
-
+        ++this.modCount;
+        Object[] prev = this.arr;
+        int to = this.size;
+        for(int i = this.size = 0; i < to; ++i) {
+            prev[i] = null;
+        }
     }
 
-    public Object get(int i) {
-        return null;
+    public Object get(int index) {
+        Objects.checkIndex(index, this.size);
+        return this.arr[index];
     }
 
-    public void sort(Comparator c) {
-
+    public boolean isEmpty() {
+        return this.size == 0;
     }
+
+    public void remove(int index) {
+        Objects.checkIndex(index, this.size);
+        Object[] es = this.arr;
+        this.fastRemove(es, index);
+    }
+
+    public void remove(Object o) {
+        Object[] es = this.arr;
+        int size = this.size;
+        int i = 0;
+        if (o == null) {
+            while(true) {
+                if (i >= size) {
+                    return;
+                }
+                if (es[i] == null) {
+                    break;
+                }
+                ++i;
+            }
+        } else {
+            while(true) {
+                if (i >= size) {
+                    return;
+                }
+                if (o.equals(es[i])) {
+                    break;
+                }
+                ++i;
+            }
+        }
+
+        this.fastRemove(es, i);
+    }
+
+    private void fastRemove(Object[] es, int i) {
+        ++this.modCount;
+        int newSize;
+        if ((newSize = this.size - 1) > i) {
+            System.arraycopy(es, i + 1, es, i, newSize - i);
+        }
+
+        es[this.size = newSize] = null;
+    }
+
+    public void sort() {
+        int expectedModCount = this.modCount;
+        this.arr = quickSort(this.arr, 0, this.size - 1);
+        if (this.modCount != expectedModCount) {
+            throw new ConcurrentModificationException();
+        } else {
+            ++this.modCount;
+        }
+    }
+
+    private Object[] quickSort(Object[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+        return arr;
+    }
+
+    private int partition(Object[] arr, int low, int high) {
+        int pivot = (int) arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if ((int) arr[j] < pivot) {
+                i++;
+                Object temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        Object temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
 }
--- CustomArrayList/src/test/java/code/NeonArrayListTest.java
+++ CustomArrayList/src/test/java/code/NeonArrayListTest.java
@@ -32,4 +32,80 @@ public class NeonArrayListTest {
     public void testSizeLines() {
         assertThat(lines.size()).isEqualTo(4);
     }
+
+    @Test
+    public void testAddByIndexNumbers() {
+        NeonArrayList<Integer> expected = new NeonArrayList<>();
+        expected.addAll(Arrays.asList(136, 17, 265, 99, 74, 203, 8, 843, 92));
+        NeonArrayList<Integer> actual = numbers;
+        actual.add(3, 99);
+        assertThat(actual.get(2)).isEqualTo(expected.get(2));
+        assertThat(actual.get(3)).isEqualTo(expected.get(3));
+        assertThat(actual.get(4)).isEqualTo(expected.get(4));
+        init();
+    }
+
+    @Test
+    public void testAddByIndexLines() {
+        NeonArrayList<String> expected = new NeonArrayList<>(4);
+        expected.addAll(Arrays.asList("first", "second", "something new", "fourth"));
+        NeonArrayList<String> actual = lines;
+        actual.add(2, "something new");
+        assertThat(actual.get(1)).isEqualTo(expected.get(1));
+        assertThat(actual.get(2)).isEqualTo(expected.get(2));
+        assertThat(actual.get(3)).isEqualTo(expected.get(3));
+        init();
+    }
+
+    @Test
+    public void testClearNumbers() {
+        numbers.clear();
+        assertThat(numbers.size()).isEqualTo(0);
+        assertThat(numbers.isEmpty()).isTrue();
+        init();
+    }
+
+    @Test
+    public void testClearLines() {
+        lines.clear();
+        assertThat(lines.size()).isEqualTo(0);
+        assertThat(lines.isEmpty()).isTrue();
+        init();
+    }
+
+    @Test
+    public void testRemoveByIndexNumbers() {
+        NeonArrayList<Integer> expected = new NeonArrayList<>();
+        expected.addAll(Arrays.asList(136, 17, 265, 74, 203, 8, 843, 92));
+        NeonArrayList<Integer> actual = numbers;
+        actual.remove(3);
+        assertThat(actual.get(2)).isEqualTo(expected.get(2));
+        assertThat(actual.get(3)).isEqualTo(expected.get(3));
+        assertThat(actual.get(4)).isEqualTo(expected.get(4));
+        init();
+    }
+
+    @Test
+    public void testRemoveByObjectLines() {
+        NeonArrayList<String> expected = new NeonArrayList<>(4);
+        expected.addAll(Arrays.asList("first", "second", "fourth"));
+        NeonArrayList<String> actual = lines;
+        actual.remove("third");
+        assertThat(actual.get(0)).isEqualTo(expected.get(0));
+        assertThat(actual.get(1)).isEqualTo(expected.get(1));
+        assertThat(actual.get(2)).isEqualTo(expected.get(2));
+        init();
+    }
+
+    @Test
+    public void testSortNumbers() {
+        NeonArrayList<Integer> expected = new NeonArrayList<>();
+        expected.addAll(Arrays.asList(8, 17, 74, 92, 136, 203, 265, 843, 893));
+        NeonArrayList<Integer> actual = numbers;
+        actual.sort();
+        assertThat(actual.get(2)).isEqualTo(expected.get(2));
+        assertThat(actual.get(3)).isEqualTo(expected.get(3));
+        assertThat(actual.get(4)).isEqualTo(expected.get(4));
+        init();
+    }
 }
